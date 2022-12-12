df978b9391232fc550ac6dfd8d210e965cc05e63
jeremymcu <10890402@me.mcu.edu.tw>
2022-12-12 17:38:35 +0800

    v3.6

--- src/project9/Main.java
+++ src/project9/Main.java
@@ -4,8 +4,6 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class Main {
-
-
     public static void main(String[] args) {
 
         int num;
@@ -17,11 +15,11 @@ public class Main {
         System.out.print("Enter number of disks: "); //Maximum number of disk is 12 due to stackoverflow on 13
         num = scan.nextInt();
 
-        long startTime = System.currentTimeMillis();
-
         for (int i=0; i<num; i++){
             stack1.push(num-i);
         }
+
+        long startTime = System.currentTimeMillis();
 
         tower towerOf = new tower();
         towerOf.hanoi(num, stack1, stack2, stack3);
